e341ddffaa48544f5971eded04225ac180a40533
Nadyne Brown <95116283+Nadyne-William@users.noreply.github.com>
2023-08-15 10:38:34 +0100

    added service and controller for course management

---
+++ src/main/java/com/management/StudentManagement/controller/CourseController.java
@@ -0,0 +1,43 @@
+package com.management.StudentManagement.controller;
+
+import com.management.StudentManagement.model.Course;
+import com.management.StudentManagement.service.CourseService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/vi/courseTesting")
+public class CourseController {
+
+    @Autowired
+    CourseService courseService;
+
+    @PostMapping()
+    public Course createCourse(Course course){
+        return courseService.createCourse(course);
+    }
+
+    @GetMapping()
+    public List<Course> getAllCourses(Course course){
+        return courseService.getAllCourses(course);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Course> getCourseById(@PathVariable(name = "id") Long courseId){
+        return courseService.getAllCoursesById(courseId);
+    }
+
+    @PutMapping("/{id}")
+    public String updateCourseDetails(@PathVariable(name = "id")Long courseID, @RequestBody @Valid Course course){
+        return courseService.updateCourseDetails(courseID, course);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteCourse(@PathVariable(name = "id")Long courseId){
+        return courseService.deleteCourseDetails(courseId);
+    }
+}
--- src/main/java/com/management/StudentManagement/service/CourseService.java
+++ src/main/java/com/management/StudentManagement/service/CourseService.java
@@ -1,12 +1,54 @@
 package com.management.StudentManagement.service;
 
+import com.management.StudentManagement.model.Course;
 import com.management.StudentManagement.repository.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CourseService {
 
     @Autowired
     CourseRepository courseRepository;
+
+    public Course createCourse(Course course){
+        return courseRepository.save(course);
+    }
+
+    public List<Course> getAllCourses(Course course){
+        return courseRepository.findAll();
+    }
+
+    public Optional<Course> getAllCoursesById(Long courseID){
+        Optional<Course> courseList = courseRepository.findById(courseID);
+        if (courseList.isPresent()){
+            return courseList;
+        }else {
+            return null;
+        }
+    }
+
+    public String updateCourseDetails(Long courseId, Course course){
+        if (courseRepository.existsById(courseId)){
+            Course newCourseDetails = courseRepository.findById(courseId).get();
+            newCourseDetails.setCourseName(course.getCourseName());
+            newCourseDetails.setCourseDescription(course.getCourseDescription());
+            newCourseDetails.setCourseCapacity(course.getCourseCapacity());
+
+            courseRepository.save(newCourseDetails);
+            return "Course details updated succesfully";
+        }
+        return "course with " + courseId + " is not available";
+    }
+
+    public String deleteCourseDetails(Long courseID){
+        if (courseRepository.existsById(courseID)){
+            courseRepository.deleteById(courseID);
+            return "Course deleted successfully";
+        }
+        return "course with " + courseID + " is not available";
+    }
 }
